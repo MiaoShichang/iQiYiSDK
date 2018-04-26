94034000e99f6dc95dac5a97186becae6608b3cc
miaoshichang <miaoshichang@126.com>
2018-04-26 14:07:07 +0800

    添加注释
    添加一些注释

--- iQiYiSDKDemo/iQiYiSDK/IQYUpLoadVideoDataModel.h
+++ iQiYiSDKDemo/iQiYiSDK/IQYUpLoadVideoDataModel.h
@@ -21,13 +21,34 @@
 
 
 // 设置下面关于要上传视频的属性
-@property (nonatomic, copy)NSString *video_extension; //扩展名 必填（mp4）
-@property (nonatomic, copy)NSString *video_size; //必填
-@property (nonatomic, strong)NSData *video_data; //必填
-@property (nonatomic, copy)NSString *video_name; // 必填
-@property (nonatomic, copy)NSString *video_description; //必填
-@property (nonatomic, copy)NSString *video_tags; // 选填
-@property (nonatomic, copy)NSString *video_type;// 选填
+
+//必填 扩展名（mp4）
+@property (nonatomic, copy)NSString *video_extension;
+
+//必填
+@property (nonatomic, copy)NSString *video_size;
+
+//必填
+@property (nonatomic, strong)NSData *video_data;
+
+//必填 -- 视频名称
+@property (nonatomic, copy)NSString *video_name;
+
+// 必填 -- 视频描述
+@property (nonatomic, copy)NSString *video_description;
+
+// 选填 -- 用户自定义标签，如"青春、搞笑"等等【以半角逗号分隔】
+@property (nonatomic, copy)NSString *video_tags;
+
+/** 选填 -- 分类 id
+  *
+  * '1' => '电影','2' => '电视剧','3' => '纪录片','4' => '动漫','5' => '音乐','6' => '综艺',
+  * '7' => '娱乐','8' => '游戏','9' => '旅游','10' => '片花','12' => '教育','13' => '时尚',
+  * '15' => '少儿','16' => '微电影','17' => '体育','20' => '广告','21' => '生活','22' => '搞笑',
+  * '24' => '财经','25' => '资讯','26' => '汽车','27' => '原创','28' => '军事' ...
+ */
+@property (nonatomic, copy)NSString *video_type;
+
 
 
 
